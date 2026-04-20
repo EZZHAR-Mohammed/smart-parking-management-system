a24a6f6be0501739d2113fe40d6f1c86c1ba0390
EZZHAR MOHAMMED <mohammedezzhar29@gmail.com>
2026-04-20 20:43:23 +0100

    the socket  frontend

---
+++ smart-parking-frontend/src/socket/index.js
@@ -0,0 +1,17 @@
+import { io } from 'socket.io-client'
+
+const socket = io('http://localhost:3000', {
+  autoConnect: false,
+  transports: ['websocket'],
+})
+
+export const connectSocket = (token) => {
+  if (token) {
+    socket.auth = { token }
+    socket.connect()
+  }
+}
+
+export const disconnectSocket = () => socket.disconnect()
+
+export default socket
